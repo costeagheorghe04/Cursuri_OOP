6f0193d58d47149330266ad692a7f7299d529188
costeagheorghe04 <costea.gheorghe.04@gmail.com>
2019-03-07 14:39:58 +0200

    Inceput

---
+++ src/CursOOP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CursOOP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
